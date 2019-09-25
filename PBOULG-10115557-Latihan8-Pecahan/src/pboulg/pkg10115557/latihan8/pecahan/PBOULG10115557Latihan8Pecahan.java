76d3eb51fbfb69d6ac64ddecd92cc32ec8480edc
Rayan <37322518+RyanJuan@users.noreply.github.com>
2019-09-25 20:12:43 +0700

    Add files via upload

---
+++ PBOULG-10115557-Latihan8-Pecahan/src/pboulg/pkg10115557/latihan8/pecahan/PBOULG10115557Latihan8Pecahan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulg.pkg10115557.latihan8.pecahan;
+
+/**
+ *
+ * @author 10115557 / rayan / PBO Ulg / pertemuan 1 / latihan 8
+ */
+public class PBOULG10115557Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    float f;
+	double d;
+	f = 1234567890.123456789F;
+	d = 1_234_567_890.123456789;
+	System.out.println("Float : " + f);
+	System.out.println("Double : " + d);
+	}
+}
